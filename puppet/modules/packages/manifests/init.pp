1815b26d6524375d3b0371e5f09a5ae9a1084813
rash <felipe@xra.sh>
2013-11-15 01:20:52 -0200

    remove the android-sdk package

--- puppet/modules/packages/manifests/init.pp
+++ puppet/modules/packages/manifests/init.pp
@@ -2,12 +2,12 @@ class packages {
   require system
 
   exec { 'pacman':
-    command => 'pacman -Sy --noconfirm --noprogressbar git feh xorg-server xorg-xinit sudo skype firefox filezilla rlwrap openssh python ruby clisp sbcl apache php bash-completion xfce4-terminal emacs nginx nitrogen fuse openbox go unzip xorg-fonts-type1 flashplugin alsa-tools dialog wpa_supplicant xclip xsel apache-ant markdown icedtea-web-java7 notify-osd virtualbox virtualbox-host-modules irssi',
+    command => 'pacman -Syy --noconfirm --noprogressbar git feh xorg-server xorg-xinit sudo skype firefox filezilla rlwrap openssh python ruby clisp sbcl apache php bash-completion xfce4-terminal emacs nginx nitrogen fuse openbox go unzip xorg-fonts-type1 flashplugin alsa-tools dialog wpa_supplicant xclip xsel apache-ant markdown icedtea-web-java7 notify-osd virtualbox virtualbox-host-modules irssi',
     timeout => 2400,
   }
 
   exec { 'yaourt':
-    command => 'yaourt -Sy --noconfirm copy-agent flite gruler android-sdk',
+    command => 'yaourt -Syy --noconfirm copy-agent flite gruler',
     timeout => 2400,
     require => Exec['pacman'],
   }
