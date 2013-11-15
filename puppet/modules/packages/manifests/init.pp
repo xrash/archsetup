d945d1b06efae6f69cef76feea6d094ee4741cb3
rash <felipe@xra.sh>
2013-11-15 02:23:40 -0200

    increase the timeout for the package manifest
    pacman and specially yaourt may take a real long time to finish

--- puppet/modules/packages/manifests/init.pp
+++ puppet/modules/packages/manifests/init.pp
@@ -3,12 +3,12 @@ class packages {
 
   exec { 'pacman':
     command => 'pacman -Syy --noconfirm --noprogressbar git feh xorg-server xorg-xinit sudo skype firefox filezilla rlwrap openssh python ruby clisp sbcl apache php bash-completion xfce4-terminal emacs nginx nitrogen fuse openbox go unzip xorg-fonts-type1 flashplugin alsa-tools dialog wpa_supplicant xclip xsel apache-ant markdown icedtea-web-java7 notify-osd virtualbox virtualbox-host-modules irssi',
-    timeout => 2400,
+    timeout => 3600,
   }
 
   exec { 'yaourt':
     command => 'yaourt -Syy --noconfirm copy-agent flite gruler',
-    timeout => 2400,
+    timeout => 3600,
     require => Exec['pacman'],
   }
 }
