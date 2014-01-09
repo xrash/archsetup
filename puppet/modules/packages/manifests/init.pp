7420cb58edc62021910db036e8fc4d34c12b7235
rash <felipe@xra.sh>
2014-01-09 07:23:45 -0200

    add the pkg evince

--- puppet/modules/packages/manifests/init.pp
+++ puppet/modules/packages/manifests/init.pp
@@ -2,7 +2,7 @@ class packages {
   include multilib
 
   exec { 'pacman':
-    command => 'pacman -Syy --noconfirm --noprogressbar git feh xorg-server xorg-xinit sudo skype firefox filezilla rlwrap openssh python ruby clisp sbcl apache php bash-completion xfce4-terminal emacs nginx nitrogen fuse openbox go unzip xorg-fonts-type1 flashplugin alsa-tools dialog wpa_supplicant xclip xsel apache-ant markdown icedtea-web-java7 notify-osd virtualbox virtualbox-host-modules irssi fortune-mod mercurial htop newsbeuter',
+    command => 'pacman -Syy --noconfirm --noprogressbar git feh xorg-server xorg-xinit sudo skype firefox filezilla rlwrap openssh python ruby clisp sbcl apache php bash-completion xfce4-terminal emacs nginx nitrogen fuse openbox go unzip xorg-fonts-type1 flashplugin alsa-tools dialog wpa_supplicant xclip xsel apache-ant markdown icedtea-web-java7 notify-osd virtualbox virtualbox-host-modules irssi fortune-mod mercurial htop newsbeuter evince',
     timeout => 3600,
     require => Exec['multilib'],
   }
